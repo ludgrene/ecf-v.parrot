493ebf3b96ae922c6884ce4e1e10d4ec11640399
ludgrene <ludgame95@gmail.com>
2024-02-20 11:15:39 +0100

    Mon commit

--- mon-site/voiture/voiture.js
+++ mon-site/voiture/voiture.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Ajouter un écouteur d'événements pour le bouton de contact
+    // d'événements pour le bouton de contact
     document.getElementById('contactButton').addEventListener('click', function() {
         alert('tel:  +33 97778887');
     });
 
-    // Ajouter des écouteurs d'événements pour l'effet de zoom
+    // pour l'effet de zoom
     let zoomableImages = document.querySelectorAll('.zoomable');
     zoomableImages.forEach(function(img) {
         img.addEventListener('click', function() {
